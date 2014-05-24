f3802a7fd0ef79e2878c927c1c205677ffa53b3c
Buganini Q <buganini@gmail.com>
2014-05-24 20:48:51 +0800

    add script for assembling rom

---
+++ rom.py
@@ -0,0 +1,32 @@
+import os
+import sys
+import re
+
+init=chr(0xff)
+#mtdparts="128k:tuboot.bin,1792k:ap121-2.6.31-squashfs,1024k:vmlinux.lzma.uImage,64k:NVRAM:,29696k:ap121-2.6.31-jffs2,64k:art.bin"
+mtdparts="128k:tuboot.bin,1792k:ap121-2.6.31-squashfs,1024k:vmlinux.lzma.uImage,64k:NVRAM:,1024k:ap121-2.6.31-jffs2,64k:art2.bin"
+
+parts=mtdparts.split(",")
+for i, part in enumerate(parts):
+	parts[i]=part.split(":")
+	sz,bs=re.match("([0-9]+)([kmg]?)", parts[i][0], re.I).groups()
+	sz=int(sz)
+	bs={"k":1024,"m":1024**2,"g":1024**3}[bs.lower()]
+	parts[i][0]=sz*bs
+	parts[i]=(parts[i][0], parts[i][1])
+
+romfile=open(sys.argv[1], "wb")
+for sz, imgfile in parts:
+	if os.path.exists(imgfile):
+		img=open(imgfile).read()
+		romfile.write(img)
+		print imgfile, len(img)
+		rest=sz-len(img)
+	else:
+		print imgfile, "(SKIPPED)"
+		rest=sz
+	if rest>0:
+		print "fill", rest
+		romfile.write(init*rest)
+
+romfile.close()
